6c1ab5822fa8066ddbeaaa2cd554343e6be1d047
NEELESH SHARMA <nsharma1@me.iitr.ac.in>
2020-03-13 10:15:33 +0530

    Updates

---
+++ precache-manifest.431dbad9bc8ebe8baab3c40962bd84ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9f05e976e96340da1511f3a124015fb",
+    "url": "/Foreign-Research-Portal/index.html"
+  },
+  {
+    "revision": "29c93e9d58ee86ba89e2",
+    "url": "/Foreign-Research-Portal/static/css/main.4ec87b06.chunk.css"
+  },
+  {
+    "revision": "eb4ee3f83023b87a3cfe",
+    "url": "/Foreign-Research-Portal/static/js/2.b7fc9cf1.chunk.js"
+  },
+  {
+    "revision": "280b48dbc3281f342da16f66e41cf46c",
+    "url": "/Foreign-Research-Portal/static/js/2.b7fc9cf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29c93e9d58ee86ba89e2",
+    "url": "/Foreign-Research-Portal/static/js/main.ce92ecc4.chunk.js"
+  },
+  {
+    "revision": "829cb499d72ae537cde4",
+    "url": "/Foreign-Research-Portal/static/js/runtime-main.d095deef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Foreign-Research-Portal/static/media/logo.5d5d9eef.svg"
+  }
+]);
