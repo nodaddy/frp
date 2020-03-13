8f86f814fe4d59d0871e242242a7bb70a6cd7f2d
NEELESH SHARMA <nsharma1@me.iitr.ac.in>
2020-03-13 10:18:24 +0530

    Updates

---
+++ precache-manifest.1dff2743682757dc666f8c8aee79e2d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e2e9d88cd3dab5f0f153b59ca405aad",
+    "url": "/frp/index.html"
+  },
+  {
+    "revision": "719d22cd036bbc927ae5",
+    "url": "/frp/static/css/main.4ec87b06.chunk.css"
+  },
+  {
+    "revision": "eb4ee3f83023b87a3cfe",
+    "url": "/frp/static/js/2.b7fc9cf1.chunk.js"
+  },
+  {
+    "revision": "280b48dbc3281f342da16f66e41cf46c",
+    "url": "/frp/static/js/2.b7fc9cf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "719d22cd036bbc927ae5",
+    "url": "/frp/static/js/main.999f955f.chunk.js"
+  },
+  {
+    "revision": "7744c6e2c1cb9cc6d6d1",
+    "url": "/frp/static/js/runtime-main.ad5fd8bf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/frp/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Foreign-Research-Portal/precache-manifest.431dbad9bc8ebe8baab3c40962bd84ed.js"
+  "/frp/precache-manifest.1dff2743682757dc666f8c8aee79e2d3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Foreign-Research-Portal/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/frp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
